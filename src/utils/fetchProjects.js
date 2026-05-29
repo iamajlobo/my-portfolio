e47668eb57874bbd1e782f63ed496e5891cff75d
iamAjLobo <aj.lobo.2004@gmail.com>
2026-05-29 23:57:20 +0800

    added new project

--- src/utils/fetchProjects.js
+++ src/utils/fetchProjects.js
@@ -1,48 +1,58 @@
 import project1 from "../assets/projects/portfolio.png";
-import project2 from "../assets/projects/lobogames.png"
-import project3 from "../assets/projects/lobomovies.png"
+import project2 from "../assets/projects/lobogames.png";
+import project3 from "../assets/projects/lobomovies.png";
 import project4 from "../assets/projects/lobodex.png";
-
+import project5 from "../assets/projects/shoplobs.png";
 
 export const projects = [
-    {
-        id: 4,
-        title: "Lobodex",
-        description:
-  "A Pokédex-inspired web app that allows users to explore Pokémon data, including stats, types, and evolution chains, with a responsive and interactive interface.",
-        image: project4,
-        tech: ["React", "Tailwind"],
-        github: "https://github.com/iamajlobo/lobodex",
-        live: "https://iamajlobo.github.io/lobodex/"
-    },
-    {
-        id: 3,
-        title: "My Portfolio",
-        description:
-            "A personal portfolio website showcasing projects, skills, and experience with a responsive and modern UI.",
-        image: project1,
-        tech: ["React", "Tailwind"],
-        github: "https://github.com/iamajlobo/my-portfolio",
-        live: "https://iamajlobo.github.io/my-portfolio/"
-    },
-    {
-        id: 2,
-        title: "Lobo Mini Games",
-        description:
-            "A collection of browser-based mini games built with JavaScript, featuring interactive gameplay and simple animations.",
-        image: project2,
-        tech: ["HTML5", "CSS", "JavaScript"],
-        github: "https://github.com/iamajlobo/mini-projects",
-        live: "https://iamajlobo.github.io/mini-projects/"
-    },
-    {
-        id: 1,
-        title: "Lobo Movies",
-        description:
-            "A movie browsing app that allows users to explore films, view details, and manage favorites using a dynamic React interface.",
-        image: project3,
-        tech: ["HTML5","CSS","React"],
-        github: "https://github.com/iamajlobo/lobo-react-movies",
-        live: "https://iamajlobo.github.io/lobo-react-movies/"
-    }
-]
+  {
+    id: 5,
+    title: "Shoplobs",
+    description:
+      "A simple e-commerce frontend application that allows users to browse products and view ratings through a clean, responsive, and interactive interface.",
+    image: project5,
+    tech: ["Frontend", "React", "Tailwind"],
+    github: "https://github.com/iamajlobo/shoplobs",
+    live: "https://iamajlobo.github.io/shoplobs/",
+  },
+  {
+    id: 4,
+    title: "Lobodex",
+    description:
+      "A Pokédex-inspired web app that allows users to explore Pokémon data, including stats, and  types, with a responsive and interactive interface.",
+    image: project4,
+    tech: ["Frontend", "React", "Tailwind"],
+    github: "https://github.com/iamajlobo/lobodex",
+    live: "https://iamajlobo.github.io/lobodex/",
+  },
+  {
+    id: 3,
+    title: "My Portfolio",
+    description:
+      "A personal portfolio website showcasing projects, skills, and experience with a responsive and modern UI.",
+    image: project1,
+    tech: ["Frontend", "React", "Tailwind"],
+    github: "https://github.com/iamajlobo/my-portfolio",
+    live: "https://iamajlobo.github.io/my-portfolio/",
+  },
+  {
+    id: 2,
+    title: "Lobo Mini Games",
+    description:
+      "A collection of browser-based mini games built with JavaScript, featuring interactive gameplay and simple animations.",
+    image: project2,
+    tech: ["HTML5", "CSS", "JavaScript"],
+    github: "https://github.com/iamajlobo/mini-projects",
+    live: "https://iamajlobo.github.io/mini-projects/",
+  },
+  {
+    id: 1,
+    title: "Lobo Movies",
+    description:
+      "A movie browsing app that allows users to explore films, view details, and manage favorites using a dynamic React interface.",
+    image: project3,
+    tech: ["HTML5", "CSS", "React"],
+    github: "https://github.com/iamajlobo/lobo-react-movies",
+    live: "https://iamajlobo.github.io/lobo-react-movies/",
+  },
+];
